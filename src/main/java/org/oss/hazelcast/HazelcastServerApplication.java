9740b82b27a2587d8ac284ac0a14a0fd1379e566
aranga-au <aranga.nanayakkara@gmail.com>
2016-06-26 15:55:31 +1000

    hazelcast config

--- src/main/java/org/oss/hazelcast/HazelcastServerApplication.java
+++ src/main/java/org/oss/hazelcast/HazelcastServerApplication.java
@@ -1,11 +1,14 @@
 package org.oss.hazelcast;
 
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HazelcastServerApplication {
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(HazelcastServerApplication.class,args);
 	}
 }
