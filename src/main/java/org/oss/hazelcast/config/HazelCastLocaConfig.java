65ddd3369e65c3672b9a93192d1d6832ec505287
aranga-au <aranga.nanayakkara@gmail.com>
2016-06-26 17:01:18 +1000

    add local config

--- src/main/java/org/oss/hazelcast/config/HazelCastLocaConfig.java
+++ src/main/java/org/oss/hazelcast/config/HazelCastLocaConfig.java
@@ -1,12 +1,19 @@
 package org.oss.hazelcast.config;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by arang on 25/06/2016.
  */
-
+@Configuration
+@ConditionalOnMissingBean(HazelCastAwsConfig.class)
 public class HazelCastLocaConfig
 {
-    /*
     @Bean
     public Config createConig()
     {
@@ -16,11 +23,11 @@ public class HazelCastLocaConfig
         c.getNetworkConfig().setJoin(new JoinConfig());
         c.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(true);
 
-        //c.getNetworkConfig().getJoin().getAwsConfig().setEnabled(true);
-        //c.getNetworkConfig().getJoin().getAwsConfig().setEnabled(true);
+        c.getNetworkConfig().getJoin().getAwsConfig().setEnabled(true);
+        c.getNetworkConfig().getJoin().getAwsConfig().setEnabled(true);
 
         return c;
     }
-*/
+
 
 }
